d11c82b58c394ed3fee01c743ec470c0f99005ed
Onkar <omkar15september@gmail.com>
2025-10-02 09:46:55 +0200

    f1

--- ios/App/widgets/AppIntent.swift
+++
@@ -1,8 +0,0 @@
-
-import WidgetKit
-import AppIntents
-
-struct ConfigurationAppIntent: WidgetConfigurationIntent {
-    static var title: LocalizedStringResource { "Medication Tracker" }
-    static var description: IntentDescription { "Track your medication schedule" }
-}
