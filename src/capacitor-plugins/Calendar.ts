d4ac042be9dac47e158a3fe81de64876b8a4bc8d
Onkar <omkar15september@gmail.com>
2025-10-08 16:28:31 +0200

    D

--- src/capacitor-plugins/Calendar.ts
+++
@@ -1,8 +0,0 @@
-import { Capacitor, registerPlugin } from '@capacitor/core';
-
-export interface CalendarPlugin {
-  openCalendar(options: { time: number }): Promise<{ success: boolean }>;
-}
-
-const Calendar = registerPlugin<CalendarPlugin>('Calendar');
-export { Calendar };
